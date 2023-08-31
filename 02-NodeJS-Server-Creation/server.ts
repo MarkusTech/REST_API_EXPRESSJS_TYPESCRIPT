28b8b43dae6ed4502db429b89b8618ee3235dd04
Markus Cabaron <55816864+MarkusTech@users.noreply.github.com>
2023-08-31 10:53:56 +0800

    os Module

--- 02-NodeJS-Server-Creation/server.ts
+++ 02-NodeJS-Server-Creation/server.ts
@@ -1,4 +1,5 @@
 import http, { Server, IncomingMessage, ServerResponse } from "http";
+import os from "os";
 
 const hostname: string = "127.0.0.1";
 const port: number = 5000;
@@ -7,9 +8,21 @@ const server: Server = http.createServer(
   (request: IncomingMessage, response: ServerResponse) => {
     response.statusCode = 200;
     response.setHeader("Content-Type", "text/html");
-    response.end(
-      `<h3 style = "font-family: lato, sans-serif; color: green">Wenn Works</h3>`
-    );
+
+    // OS MODULE
+    let osData = {
+      totalMemory: os.totalmem(),
+      freeMemory: os.freemem(),
+      homedir: os.homedir(),
+      computerName: os.hostname(),
+    };
+
+    // JSON.stringify
+    response.end(`<pre>${JSON.stringify(osData)}</pre>`);
+
+    // response.end(
+    //   `<h3 style = "font-family: lato, sans-serif; color: green">Wenn Works</h3>`
+    // );
   }
 );
 
