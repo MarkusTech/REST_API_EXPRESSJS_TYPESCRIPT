5381bcdc696fc63ba3bb715ed0b5954983b4233d
Markus Cabaron <55816864+MarkusTech@users.noreply.github.com>
2023-08-31 10:25:24 +0800

    nodejs basics

---
+++ 01-Node-JS-Basics/app.ts
@@ -0,0 +1,16 @@
+let courseName: string = "Backend web development";
+console.log(courseName);
+
+const printNumber = (start: number, end: number): void => {
+  let temp: string = "";
+  if (start < end) {
+    for (let i: number = start; i <= end; i++) {
+      temp += `${i}`;
+    }
+    console.log(temp);
+  } else {
+    console.log(`Invalid numbers`);
+  }
+};
+
+printNumber(5, 10);
